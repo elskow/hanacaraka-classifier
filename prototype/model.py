cd8f6bc78510d6c62dc692c90127d1b7b0e8ce5a
NaufalF121 <blueshoko@gmail.com>
2024-05-18 21:44:51 +0700

    update model

--- prototype/model.py
+++ prototype/model.py
@@ -2,20 +2,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Model(nn.Module):
+class CNNModel(nn.Module):
     def __init__(self):
-        super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        super(CNNModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(64 * 7 * 7, 500)
-        self.fc2 = nn.Linear(500, 10)
-        
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(32 * 56 * 56, 500)
+        self.fc2 = nn.Linear(500, 20)
+
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 64 * 7 * 7)
+        x = x.view(-1, 32 * 56 * 56)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
-    
