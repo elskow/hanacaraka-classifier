b51612f9e6e8fd6844873bd5fb5ce0528c1c2493
NaufalF121 <blueshoko@gmail.com>
2024-05-19 16:15:25 +0700

    adding dropout

--- prototype/model.py
+++ prototype/model.py
@@ -8,12 +8,15 @@ class CNNModel(nn.Module):
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.dropout = nn.Dropout(0.5)
         self.fc1 = nn.Linear(32 * 56 * 56, 500)
         self.fc2 = nn.Linear(500, 20)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
+        x = self.dropout(x)
         x = self.pool(F.relu(self.conv2(x)))
+        x = self.dropout(x)
         x = x.view(-1, 32 * 56 * 56)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
