8d41520eb460886718d3c1690848a461673315ea
NaufalF121 <blueshoko@gmail.com>
2024-05-19 19:28:21 +0700

    custom model

--- prototype/model.py
+++ prototype/model.py
@@ -5,20 +5,26 @@ import torch.nn.functional as F
 class CNNModel(nn.Module):
     def __init__(self):
         super(CNNModel, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.dropout = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(32 * 56 * 56, 500)
-        self.fc2 = nn.Linear(500, 20)
+        self.conv1 = nn.Conv2d(3, 128, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d( kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1)
+        self.dropout = nn.Dropout()
+        self.fc1 = nn.Linear(576, 32)
+        self.fc2 = nn.Linear(32, 20)
+        self.flatten = nn.Flatten()
+        
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.dropout(x)
         x = self.pool(F.relu(self.conv2(x)))
-        x = self.dropout(x)
-        x = x.view(-1, 32 * 56 * 56)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.flatten(x)
+       
         x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
-        return x
+        return F.log_softmax(x, dim=1)
 
